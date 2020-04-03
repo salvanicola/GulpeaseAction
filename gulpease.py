59d91e84f945fa3d7fe002239850c2b7ad318807
Gianmarco Pettinato <gianmarco.pettinato@gmail.com>
2020-04-03 17:12:00 +0200

    testing 2

--- gulpease.py
+++ gulpease.py
@@ -9,7 +9,7 @@ nf = sys.argv[1]
 testo = str(textract.process(nf))
 
 parole  = len(re.findall(r'\w+', testo))
-lettere = len(re.findall(r'\w', testo))
+lettere = len(re.findall(r'\w', testo)) + len(re.findall(r'\W',testo))
 punti = len(re.findall('[.]+\s', testo) - len(re.findall('[.]+\s+[.]', testo)))
 
 indiceG=89+((300*punti)-(10*lettere))/parole
