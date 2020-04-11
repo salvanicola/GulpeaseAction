be6cbc3409c52224d8089c04dfcb114ef45df94b
nicola <nicola.salvadore@gmail.com>
2020-04-11 11:30:40 +0200

    aggiunto cheat per saltare gli indici

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ mkdir Gulpease
 ls *.pdf > Gulpease/pdfList.txt
 while IFS= read -r document || [ -n "$document" ]
 do
-    pdftotext $document
+    pdftotext -f 4 $document
     python /gulpease.py ${document%.*}.txt > Gulpease/${document%.*}-eval.txt
 done < "Gulpease/pdfList.txt"
 
